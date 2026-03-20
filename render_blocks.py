d3c20ce678dc7396752ea67aab8d5a5edea495a2
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-03-20 23:10:55 +0100

    minor performance boost

--- render_blocks.py
+++ render_blocks.py
@@ -6,17 +6,39 @@ from PIL import Image, ImageOps
 GRADES = ['3', '4', '5A', '5B', '5C', '6A', '6A+', '6B', '6B+', '6C', '6C+', '7A', '7A+', '7B', '7B+', '7C', '7C+', '8A', '8A+', '8B', '8B+', '8C', '8C+', '9A']
 IMAGE_PATH = "img/"
 
+# Cache per le immagini
+@st.cache_resource
+def load_image_cached(image_path):
+    """Carica e cache le immagini per evitare ricaricamenti multipli"""
+    try:
+        img = Image.open(image_path)
+        img = ImageOps.exif_transpose(img)
+        return img
+    except Exception as e:
+        st.error(f"Errore nel caricamento dell'immagine {image_path}: {e}")
+        return None
+
 @st.fragment
 def render_block_list(boulder_data, settori, grade_to_int, min_grade, max_grade, tags):
 
     st.subheader(f"Lista dei blocchi")
     selected_sector = st.selectbox("Scegli un settore", options=settori, index=0)
 
-    if st.checkbox("Mostra statistiche per nerd"):
-        if selected_sector == "Tutti":
-            st.bar_chart(boulder_data['grado'].value_counts().sort_index())
-        else:
-            st.bar_chart(boulder_data[boulder_data['settore'] == selected_sector]['grado'].value_counts().sort_index())
+    @st.fragment
+    def nerd_stats(selected_sector):
+        if st.checkbox("Mostra statistiche per nerd"):
+            @st.cache_data
+            def histogram_by_sector(selected_sector):
+                if selected_sector == "Tutti":
+                    return boulder_data['grado'].value_counts().sort_index()
+                else:
+                    return boulder_data[boulder_data['settore'] == selected_sector]['grado'].value_counts().sort_index()
+            if selected_sector == "Tutti":
+                st.bar_chart(histogram_by_sector(selected_sector))
+            else:
+                st.bar_chart(histogram_by_sector(selected_sector))
+    
+    nerd_stats(selected_sector)
 
     col1, col2 = st.columns([2, 3])
     with col1:
@@ -77,9 +99,9 @@ def render_block_list(boulder_data, settori, grade_to_int, min_grade, max_grade,
                 if pd.notna(img_name):
                     if pd.notna(faccia):
                         st.write(f"Faccia: {int(faccia)}")
-                    image = Image.open(os.path.join(IMAGE_PATH, img_name))
-                    image = ImageOps.exif_transpose(image) 
-                    st.image(image, width="content")
+                    image = load_image_cached(os.path.join(IMAGE_PATH, img_name))
+                    if image:
+                        st.image(image, width="content")
                     
                     for _, blocco in vie_faccia.iterrows():
                         with st.expander(f'**{blocco["numero"]}** - {blocco["nome"]} - {blocco["grado"]} - {blocco['partenza']}'):
